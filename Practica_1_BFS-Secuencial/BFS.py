c2f8d4ad7008ce689220570a4191322de8d1d99e
Ana Lilia <ana_li84@ciencias.unam.mx>
2024-09-05 19:21:23 -0600

    implementación de BFS

--- Practica_1_BFS-Secuencial/BFS.py
+++ Practica_1_BFS-Secuencial/BFS.py
@@ -10,12 +10,35 @@ grafica = {
   'G' : ['F', 'B'],
   'H' : ['F', 'D'],
   'I' : ['F']
-}  
-cola = queue.Queue()
+}
 
+#Inicialización de cola y lista de visitados y resultado.
+cola = queue.Queue()
+visitados = []
+bfs_resultado = []
+
+# Funcion que indica si el elemento ya fue visitado
+def visitado(nodo):
+    return nodo in visitados
+    
 
 # BFS algorithm
 def bfs(grafica, nodo_inicio):
+    cola.put(nodo_inicio)                 
+    visitados.append(nodo_inicio)          
+    
+    while not cola.empty():
+        nodo_actual = cola.get()           
+        bfs_resultado.append(nodo_actual)
+        
+        for vecino in grafica[nodo_actual]:
+            if not visitado(vecino):
+                cola.put(vecino)     
+                visitados.append(vecino) 
+
+    return bfs_resultado
+    
 
 # Llamamos a la funcion bfs
-bfs(grafica, 'A')
+resultado = bfs(grafica, 'A')
+print(resultado)
